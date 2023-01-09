a12f531ac239add114c0fbd231409ea820d3361d
David Zeman <david.zeman@outlook.com>
2023-01-09 22:17:56 +0100

    Add project files.

---
+++ DARP/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DARP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static Brush CreateGridBrush(Rect bounds, Size tileSize)
+        {
+            var gridColor = Brushes.Black;
+            var gridThickness = 0.5;
+            var tileRect = new Rect(tileSize);
+
+            var gridTile = new DrawingBrush
+            {
+                Stretch = Stretch.None,
+                TileMode = TileMode.Tile,
+                Viewport = tileRect,
+                ViewportUnits = BrushMappingMode.Absolute,
+                Drawing = new GeometryDrawing
+                {
+                    Pen = new Pen(gridColor, gridThickness),
+                    Geometry = new GeometryGroup
+                    {
+                        Children = new GeometryCollection
+                        {
+                            new RectangleGeometry(new Rect(tileSize))
+                        }
+                    }
+                }
+            };
+
+            var offsetGrid = new DrawingBrush
+            {
+                Stretch = Stretch.None,
+                AlignmentX = AlignmentX.Left,
+                AlignmentY = AlignmentY.Top,
+                Transform = new TranslateTransform(bounds.Left, bounds.Top),
+                Drawing = new GeometryDrawing
+                {
+                    Geometry = new RectangleGeometry(new Rect(bounds.Size)),
+                    Brush = gridTile
+                }
+            };
+
+            return offsetGrid;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            canvas.Background = CreateGridBrush(new Rect(0, 0, canvas.RenderSize.Width, canvas.RenderSize.Height), new Size(20, 20));
+        }
+    }
+}
