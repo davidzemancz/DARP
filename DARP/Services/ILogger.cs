0b2d06e4e4e84db863eac55d4a5cd1b1c793d0e9
David Zeman <david.zeman@outlook.com>
2023-01-12 13:41:22 +0100

    Log

---
+++ DARP/Services/ILogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DARP.Services
+{
+    public interface ILogger
+    {
+        void Debug(string message);
+        void Info(string message);
+        void Warn(string message);
+        void Error(string message);
+        void Fatal(string message);
+        void Log(LogLevel level, string message);
+    }
+
+    public enum LogLevel
+    {
+        Debug = 0,
+        Info = 1,
+        Warn = 2,
+        Error = 3,
+        Fatal = 4,
+    }
+}
---
+++ DARP/Services/LoggerBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DARP.Services
+{
+    public class LoggerBase : ILogger
+    {
+        public virtual IEnumerable<TextWriter> TextWriters => new TextWriter[] {Console.Out};
+
+        public void Debug(string message)
+        {
+            Log(LogLevel.Debug, message);
+        }
+
+        public void Error(string message)
+        {
+            Log(LogLevel.Error, message);
+        }
+
+        public void Fatal(string message)
+        {
+            Log(LogLevel.Fatal, message);
+        }
+
+        public void Info(string message)
+        {
+            Log(LogLevel.Info, message);
+        }
+
+        public void Log(LogLevel level, string message)
+        {
+            foreach(TextWriter writer in TextWriters)
+            {
+                writer.WriteLine($"{DateTime.Now.ToString("yyyyMMddHHmmss")} {level} {message}");
+            }
+        }
+
+        public void Warn(string message)
+        {
+            Log(LogLevel.Warn, message);
+        }
+    }
+}
